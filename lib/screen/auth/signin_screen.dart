a20914abd047889695474917232aeb72899704c9
kbs0093 <rhqjatjr0093@gmail.com>
2024-06-20 09:29:17 +0900

    업데이트

--- lib/screen/auth/signin_screen.dart
+++ lib/screen/auth/signin_screen.dart
@@ -75,7 +75,51 @@ class SignInState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body : Container(
+        width: MediaQuery.of(context).size.width,
+        height : MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        decoration: const BoxDecoration(
+          color : Color(0xFFFFFFFF),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            const SizedBox.shrink(),
+
+            const Text(
+              "원 비전",
+              style : TextStyle(
+                color : Color(0xFF000000),
+                fontSize: 20,
+                fontWeight: FontWeight.w700
+              )
+            ),
+
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/auth/apple_signin.png",
+                  width : MediaQuery.of(context).size.width - 120,
+                  fit: BoxFit.fitWidth,
+                ),
+
+                const SizedBox(height: 20,),
+
+                Image.asset(
+                  "assets/images/auth/google_signin.png",
+                  width : MediaQuery.of(context).size.width - 120,
+                  fit: BoxFit.fitWidth,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }  
 }
